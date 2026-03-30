ff9c688768c30807365ae9ab149cec4b1ab5e028
FABLAB <tommaso.bosisio@mpolo.it>
2026-03-30 12:32:08 +0200

    push versione corretta

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,4 +1,4 @@
-/* USER CODE BEGIN Header */  GAY TOMMASO
+/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
   * @file           : main.c
@@ -101,18 +101,11 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-<<<<<<< Updated upstream
 	 Server_Start(); //CTL la funzione analizza il traffico dati dall'ESP8266, identifica i comandi URL e controlla lo stato del LED su GPIOA Pin 5.
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-=======
-	Server_Start(); //CTL la funzione analizza il traffico dati dall'ESP8266, identifica i comandi URL e controlla lo stato del LED su GPIOA Pin 5.
-    /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
-
->>>>>>> Stashed changes
   }
   /* USER CODE END 3 */
 }
